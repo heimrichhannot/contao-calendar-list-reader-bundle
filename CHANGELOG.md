e6055a604e21b22e280b303727b03ef6233add71
Thomas Körner <t.koerner@heimrich-hannot.de>
2025-01-31 09:30:41 +0100

    fixed issues with data structure and twig interop

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,9 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.1.1] - 2025-01-31
+- Fixed: calendar adjustments not correctly applied
+- Fixed: issues with twig and closures
+
 ## [0.1.0] - 2024-12-18
 Initial version.
--- src/Contao/EventBuilder.php
+++ src/Contao/EventBuilder.php
@@ -3,7 +3,9 @@
 namespace HeimrichHannot\CalendarListReaderBundle\Contao;
 
 use Contao\CalendarEventsModel;
+use Contao\CoreBundle\Twig\Interop\ContextFactory;
 use Contao\Events;
+use Contao\FrontendTemplate;
 
 class EventBuilder extends Events
 {
@@ -31,10 +33,17 @@ class EventBuilder extends Events
         }
 
         $events = $this->arrEvents[array_key_first($this->arrEvents)];
+        $events = $events[array_key_first($events)];
         $item = array_merge(
             $item,
             $events[array_key_first($events)]
         );
+
+        $template = new FrontendTemplate();
+        $template->setData($item);
+
+        $contextFactory = new ContextFactory();
+        $item = $contextFactory->fromContaoTemplate($template);
     }
 
     protected function compile(): void
